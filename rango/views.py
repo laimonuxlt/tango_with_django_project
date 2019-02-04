8f602ba0779d4e908ff017efe12cee3e9daab03f
Laimonas Samalius <2424831s@student.gla.ac.uk>
2019-02-04 00:55:35 +0000

    chapter 5.8 correction

--- rango/views.py
+++ rango/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from rango.models import Category
 
 from django.http import HttpResponse
 
@@ -13,7 +14,15 @@ def about(request):
     return render(request, 'rango/about.html',{})
     
 def index(request):
-    context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
-    return render (request, 'rango/index.html', context=context_dict)
+    #context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
+    #return render (request, 'rango/index.html', context=context_dict)
 
-
+# Query the database for a list of ALL categories currently stored.
+# Order the categories by no. likes in descending order.
+# Retrieve the top 5 only - or all if less than 5.
+# Place the list in our context_dict dictionary
+# that will be passed to the template engine.
+    category_list = Category.objects.order_by('-likes')[:5]
+    context_dict = {'categories': category_list}
+# Render the response and send it back!
+    return render(request, 'rango/index.html', context_dict)
